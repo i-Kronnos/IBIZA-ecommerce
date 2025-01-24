deba9d5f1e56f44a3d5c569b55d097b34afe8b41
Kronnos <kronnosfreelancer00@gmail.com>
2025-01-24 11:45:44 -0500

    9. (home page responsive design upgraded)

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const iconBar = document.getElementById('menu-bar');
 const navBar = document.getElementById('navbar');
 const closeMbMenu = document.getElementById('close-mb-menu');
 
+// ↓↓↓ Close mobile menu action ↓↓↓
 if (iconBar) {
     iconBar.addEventListener('click', () => {
         navBar.classList.add('active')
