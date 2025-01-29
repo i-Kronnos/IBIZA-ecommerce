b9b303d97adf4392e987f49771b15623688ff51d
Kronnos <kronnosfreelancer00@gmail.com>
2025-01-28 23:56:33 -0500

    12. (cart, contact page and alta form finished) FINAL PART 1)

---
+++ js/contacto-app.js
@@ -0,0 +1,12 @@
+// ------------------ JavaScript code for Esteban Quiñones ------------------
+
+// ---------------- Contact Form ------------------
+
+// Variables
+
+const enviarBtn = document.querySelector('#contact-form form button');
+
+// ------------------------------------------------
+/* enviarBtn.addEventListener('click', e => {
+    e.preventDefault()
+}); */
